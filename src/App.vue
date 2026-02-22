18b601d5bcd9392f9f643044a2e8a7dbd90f1741
Vicente Lineros Pardo <vlinerospardo@gmail.com>
2026-02-21 21:11:56 -0300

    fix: error http

--- src/App.vue
+++ src/App.vue
@@ -69,6 +69,12 @@ export default {
             initialized: true,
             playingInThisDevice,
           });
+        } else {
+          playerStore.$patch({
+            localMode: false,
+            initialized: true,
+            playingInThisDevice: true,
+          });
         }
 
         let deviceUuid = localStorage.getItem('device_uuid');
